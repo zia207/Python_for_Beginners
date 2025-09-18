80751e51908f923cbf79df5391b79ebd220095e3
Zia Ahmed <zia207@gmail.com>
2025-09-18 16:10:38 -0400

    Edit file link

--- Notebook/file_upload.py
+++ Notebook/file_upload.py
@@ -1,10 +1,12 @@
 
-!pip install nbformat
-
 import nbformat
-import os
-for file in os.listdir('.'):
-    if file.endswith('.ipynb'):
-        with open(file) as f:
-            nb = nbformat.read(f, as_version=4)
-            print(f"Loaded notebook: {file}")
+from nbclient import NotebookClient
+
+# Load notebook
+with open('01_06_02_functional_programming_toolz_python.ipynb') as f:
+    nb = nbformat.read(f, as_version=4)
+
+# Execute it
+client = NotebookClient(nb, timeout=600)
+client.execute()
+
